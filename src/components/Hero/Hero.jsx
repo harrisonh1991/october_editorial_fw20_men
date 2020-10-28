8340f2cc57ef06225962de4ec7419d01778164b0
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2020-10-28 14:14:23 +0800

    smoothinteractions Refactor Hero and use default scroll magic

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
 import * as ScrollMagic from "scrollmagic";
 import styles from './styles/hero.module.scss';
 import Content from '../Content/content.jsx';
-import { TweenMax, TimelineMax } from "gsap";
+import { TweenMax, TimelineMax, Sine } from "gsap";
 
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
 const Hero = props => {
 
     const { banner, title, content } = props;
+    const containerRef = useRef(null);
+
+    let controller, tl;
+
+    useEffect(() => {
+        controller = new ScrollMagic.Controller();
+        tl = new TimelineMax();
+        //tl.to(containerRef.current, 4, {rotation: 100, ease: Sine.Out });
+
+        new ScrollMagic.Scene({
+            triggerElement: containerRef.current,
+            triggerHook: "onLeave",
+            duration: "100%",
+          })
+            .setPin(containerRef.current)
+            .on("progress", onPinScrollProgress)
+            .addTo(controller);
+    }, []);
+
+
+    function onPinScrollProgress(e) {
+        let scaleProgress = ((e.progress / 0.8) * 0.75) + 0.25;
+        let yProgress = (1 - (e.progress/ 0.8)) * 30;
+
+        let progress = e.progress / 0.8;
+        console.log(scaleProgress.toFixed(2));
+
+        if(progress > 1) {
+            progress = 1;
+        }
+
+        if(scaleProgress > 1) {
+            scaleProgress = 1;
+        }
+
+        if(yProgress < 0) {
+            yProgress = 0;
+        }
+     
+        TweenMax.to(containerRef.current, 0.2, { scale: scaleProgress, rotation: progress * 360, y: yProgress*-1 + "%", ease: Sine.Out });
+        
+    }
 
     return (
-        <header>
-            <div className={styles.container}>
-                {banner}
-                <Content title={title} content={content}/>
-            </div>
-        </header>
+        <div className={styles.background}>
+            <header className={styles['header-position']} ref={containerRef} style={{ transform: 'translate(0, -30%) scale(0.25)' }}>
+                <div className={styles.container}>
+                    {banner}
+                    <div className={styles.inner}>
+                        <Content title={title} content={content}/>
+                    </div>
+                </div>
+            </header>
+        </div>
+        
     );
 
 }
