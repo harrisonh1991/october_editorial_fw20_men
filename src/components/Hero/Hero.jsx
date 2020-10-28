172dbc60dc309d4c7677b389c3dc1632f76594a8
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2020-10-28 11:05:10 +0800

    smoothinteractions Add dependencies to non delcarative scrollmagic

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+import * as ScrollMagic from "scrollmagic";
 import styles from './styles/hero.module.scss';
 import Content from '../Content/content.jsx';
+import { TweenMax, TimelineMax } from "gsap";
+
+ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
 const Hero = props => {
 
